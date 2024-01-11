399d319015e3ea5021ded0931a168b06099ed62d
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2024-01-11 16:33:21 +0530

    Add files via upload

---
+++ src/components/GetImg.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react'
+import axios from "axios"
+import { toast } from "react-toastify"
+
+function GetImg() {
+  const [img, setImg] = useState([]);
+
+  const getData = async () => {
+    try {
+      let res = await axios.get("http://localhost:5000/api/getimg");
+      setImg(res.data.data)
+
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+  function handleDelete(id) {
+    axios.delete(`http://localhost:5000/api/del/${id}`)
+      .then(() => {
+        toast.success("Delete successfully...")
+        getData();
+      });
+  }
+
+  useEffect(() => {
+
+    getData();
+
+  }, [])
+
+
+  return (
+    <div className='container row d-flex justify-content-center mx-3 align-items-center'>
+
+
+        {img.map((eachData, i) => {
+
+          return(
+            
+            <div key={i} className="card card_style" style={{width:"15rem",height:"15rem",padding:"10px",margin:"20px 15px"}}>
+                <img height={200} src={`http://localhost:5000/img/${eachData.image}`} alt="..." />
+                <button className='btn btn-warning' onClick={() => handleDelete(eachData._id)}>Delete</button>
+
+            </div>  
+
+                   
+          )
+      
+        })
+       
+        }
+    
+
+    </div>
+  )
+}
+
+export default GetImg
---
+++ src/components/Upload.js
@@ -0,0 +1,42 @@
+import React, { useState ,useEffect} from 'react'
+import axios from "axios"
+import GetImg from './GetImg'
+import { toast } from "react-toastify"
+
+function Upload() {
+
+    const [file, setFile] = useState(null)
+
+    const uploadImage = () => {
+        const formData = new FormData()
+        formData.append("file", file)
+
+        axios.post("http://localhost:5000/api/upload", formData)
+            .then((res) => {
+                console.log(res.data);
+            //    toast.success("Upload successfully 😍 ");
+               location.reload();
+            })
+
+    }
+
+
+
+    return (
+        <div> 
+         
+         <h2> Upload Image</h2>
+            <div className='img-container'>
+           
+              <input type="file" name='file' onChange={(e) => setFile(e.target.files[0])} /> <br/>
+               
+                <button className='btn btn-warning' onClick={uploadImage}>Upload </button>
+              
+            </div>
+
+            <GetImg/>
+        </div>
+    )
+}
+
+export default Upload;
