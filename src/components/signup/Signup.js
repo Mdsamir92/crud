79de1b8267aba7d4d34b21bba9ebe45ecbacbe7c
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2024-01-20 16:51:40 +0530

    Update Signup.js

--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -26,6 +26,10 @@ const navigate = useNavigate("");
      toast.error("Name requires a minimum of 3 characters");
      return;
    }
+      else if(email.length<13){
+     toast.error("Type valid email");
+     return;
+   }
    else if (password.length < 5) {
      toast.error("Password requires a minimum of 5 characters");
      return ;
@@ -55,10 +59,7 @@ const navigate = useNavigate("");
      }
 })
 
-
-
 }
-
 
   return (
     <div className='form-container'>
@@ -72,13 +73,13 @@ const navigate = useNavigate("");
       </div>
       <form>
         <label>Name:</label> <br />
-        <input type="text" value={name} className="form-control" placeholder="enter name" onChange={(e) => setName(e.target.value)} />
+        <input type="text" value={name}  placeholder="enter name" onChange={(e) => setName(e.target.value)} />
         <br />
         <label>Email:</label> <br />
-        <input type="email" value={email} className="form-control" placeholder="enter email" onChange={(e) => setEmail(e.target.value)} />
+        <input type="email" value={email}  placeholder="enter email" onChange={(e) => setEmail(e.target.value)} />
         <br />
         <label>Password:</label> <br />
-        <input type="password" value={password} className="form-control" placeholder="enter password" onChange={(e) => setPassword(e.target.value)} />
+        <input type="password" value={password}  placeholder="enter password" onChange={(e) => setPassword(e.target.value)} />
         <br />
         {/* <p style={{ color: "red" }}>{error && error}</p> */}
        
