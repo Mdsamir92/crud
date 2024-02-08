292bd78151ed1a6cb434bfb4d2b5fee3d0d7dcfb
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2024-02-08 12:36:04 +0530

    Update Login.js

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -4,9 +4,8 @@ import { useNavigate, Link } from 'react-router-dom';
 import axios from "axios"
 import { toast } from "react-toastify"
 
+
 function LoginForm() {
-
-
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -16,10 +15,11 @@ function LoginForm() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    
     try {
       const res = await axios.post("https://login-register-form-go9w.onrender.com/login",
         { email, password },)
-
 
       if (res.data) {
         localStorage.setItem('token', res.data.token);
@@ -40,32 +40,33 @@ function LoginForm() {
 
     }
 
-
   }
 
 
   return (
-    <div className='form-container'>
-      <h2>SignIn</h2>
+    <div className='form-container '>
+      <h2>Login💻</h2>
 
-      <div class="social-icons">
-        <a href="#" ><i class="fa-brands fa-google"></i> </a>
-        <a href="#" ><i class="fa-brands fa-facebook"></i> </a>
-        <a href="#" ><i class="fa-brands fa-github"></i> </a>
-       
-      </div>
+{/*        
+      <div className="social-icons">
+        <a href="#" ><i className="fa-brands fa-google"></i> </a>
+        <a href="#" ><i className="fa-brands fa-facebook"></i> </a>
+        <a href="#" ><i className="fa-brands fa-github"></i> </a> 
+      </div> */}
+
       <form>
         <label>Email:</label> <br />
-        <input type="text" value={email} required placeholder="enter email" onChange={(e) => setEmail(e.target.value)} />
+        <input type="text" value={email} required  placeholder="enter email" onChange={(e) => setEmail(e.target.value)} />
         <br />
         <label>Password:</label> <br />
         <input type="password" value={password} required placeholder="enter password" onChange={(e) => setPassword(e.target.value)} />
         <br />
+        <input class="checkbox" type="checkbox" required /> Keep me signed in 
         <button type="submit" onClick={handleSubmit} className='login-btn'>Login</button>
        
       </form>
-      <br />
-      <Link to="/signup">New user signup here</Link>
+ 
+      <Link to="/signup">Dont't have an account ? signup here</Link>
     </div>
   );
 }
