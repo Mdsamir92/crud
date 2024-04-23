4cf870786ad9e97c67d56aeb9bd048900e54aa86
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2024-04-23 20:52:33 +0530

    Update Create.js

--- src/Create.js
+++ src/Create.js
@@ -11,29 +11,30 @@ function Create() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
-
+  //  const [isValid,setisValid] = useState(false)
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if(!name || !email || !phone){
-      Swal.fire({icon:"error",title:"Oops",text:"please fill data..."})
-     return;
-    }   else if(name.length<3){
-      Swal.fire({icon:"error",title:"Oops",text:"Name requires a minimum of 3 characters"});
+    if (!name || !email || !phone) {
+      Swal.fire({ icon: "error", title: "Oops", text: "please fill data..." })
       return;
-    }else if (email.length < 13) { 
-      Swal.fire({icon:"error",title:"Oops",text:"Enter a valid email address"});
+    } else if (name.length < 3) {
+      Swal.fire({ icon: "error", title: "Oops", text: "Name requires a minimum of 3 characters" });
+      return;
+    } else if (email.length < 13) {
+      Swal.fire({ icon: "error", title: "Oops", text: "Enter a valid email address" });
       return;
     }
     else if (phone.length < 10) {
-      Swal.fire({icon:"error",title:"Oops",text:"number requires a minimum of 10  digits"});
-      return ;
+      Swal.fire({ icon: "error", title: "Oops", text: "number requires a minimum of 10  digits" });
+      return;
     }
-    else{
-      Swal.fire({icon:"success",title:"wow",text:"submit successfully..."})
+    else {
+      Swal.fire({ icon: "success", title: "wow", text: "submit successfully..." })
+
     }
- 
+
     axios.post(
       'https://sam-crud.onrender.com/api/create',
       {
@@ -42,8 +43,11 @@ function Create() {
         phone: phone,
       }
     ).then(() => {
+      // alert("submit success")
+      // location.reload();
       navigate("/read");
     });
+
 
   }
 
@@ -54,35 +58,36 @@ function Create() {
 
 
         <div >
-        <Link to="/read">
-          <button className='btn btn-warning mx-3'>Read Data</button>
-        </Link>
-        <Link to="/upload">
-          <button className='btn btn-warning'>Upload Image </button>
-        </Link>
+          <Link to="/read">
+            <button className='btn btn-warning mx-3'>Read Data</button>
+          </Link>
+          <Link to="/upload">
+            <button className='btn btn-warning'>Upload Image </button>
+          </Link>
         </div>
-     
+
       </div>
 
-       <div className='form-container'>
-       <h2>Create Data</h2>
+      <div className='form-container'>
+        <h2>Create Data</h2>
         <form >
           <div>
-            <label className="form-label">Name</label> <br/>
-            <input type="name"  placeholder='Enter name...' onChange={(e) => setName(e.target.value)}  required/> 
+            <label className="form-label">Name</label> <br />
+            <input type="name" placeholder='Enter name...' onChange={(e) => setName(e.target.value)} required />
           </div>
           <div>
             <label className="form-label"  >Email address</label>
-            <input type="email"  placeholder='Enter email...' onChange={(e) => setEmail(e.target.value)} required  />
+            <input type="email" placeholder='Enter email...' onChange={(e) => setEmail(e.target.value)} required />
           </div>
           <div>
-            <label className="form-label">Phone </label> <br/>
-            <input type="number"  placeholder='Enter number...' onChange={(e) => setPhone(e.target.value)} required  />
+            <label className="form-label">Phone </label> <br />
+             <input type="number" placeholder='Enter number...' value={phone} onChange={(e) =>setPhone(e.target.value.slice(0,10))} required />
           </div>
-     
+    
+
           <button type="submit" className='reg-btn' onClick={handleSubmit}>Submit</button>
         </form>
-       
+
       </div>
     </div>
   )
